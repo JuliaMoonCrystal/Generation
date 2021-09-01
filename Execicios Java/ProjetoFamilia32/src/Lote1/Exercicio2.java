354a11a25575311023cb9e43c2bdfd32170a4b2e
julia <jufarias20002@gmail.com>
2021-09-01 14:46:57 -0300

    exercicio 2

--- Execicios Java/ProjetoFamilia32/src/Lote1/Exercicio2.java
+++ Execicios Java/ProjetoFamilia32/src/Lote1/Exercicio2.java
@@ -18,23 +18,35 @@ public class Exercicio2 {
 		System.out.println("\nDigite um número : ");
 		num3=scan.nextInt();
 
-		if(num1 > num2) {
-			aux= num1;
-			num1=num2;
-			num2=aux;
+		if(num1 < num2 && num1 < num3) {
+			if(num2 < num3) {
+				System.out.println("\n Os números em ordem crescente :"+num1+" - "+num2+" - "+num3);
+			}
+			else {
+				System.out.println("\n Os números em ordem crescente :"+num1+" - "+num3+" - "+num2);
+
+			}
 		}
-		else if (num2 > num3) {
-			aux=num2;
-			num2 =num3;
-			num3=aux;
+		else if(num2 < num3 && num2 < num3) {
+			 if(num1 < num3) {
+					System.out.println("\n Os números em ordem crescente :"+num2+" - "+num1+" - "+num3);
+			 }
+			 else {
+					System.out.println("\n Os números em ordem crescente :"+num1+" - "+num3+" - "+num1);
+
+			 }
 		}
-		else if(num3 > num1) {
-			aux=num3;
-			num1=num3;
-			num3= aux;
+		else if(num3 < num1 && num3 < num2) {
+			if(num1 < num2) {
+				   System.out.println("\n Os números em ordem crescente :"+num3+" - "+num1+" - "+num2);
+			}
+			else {
+				System.out.println("\n Os números em ordem crescente :"+num3+" - "+num2+" - "+num1);
+			}
 		}
-		
-		System.out.println("\n Os números em ordem crescente :"+num1+" - "+num2+" - "+num3);
+		else {
+			System.out.println("Acabou");
+		}
 	}
 
 }
