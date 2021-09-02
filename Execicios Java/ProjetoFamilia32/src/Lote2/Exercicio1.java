ed5bb8e13358260ac9226e0a2ebeed870453b78c
julia <jufarias20002@gmail.com>
2021-09-02 10:03:12 -0300

    exercicio 1

---
+++ Execicios Java/ProjetoFamilia32/src/Lote2/Exercicio1.java
@@ -0,0 +1,15 @@
+package Lote2;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		for(int x=1000; x <=1999;x++) {
+			if(x % 11 == 5) {
+				System.out.println("\nO número : "+x+ " tem resto 5");
+			}
+		}
+
+	}
+
+}
