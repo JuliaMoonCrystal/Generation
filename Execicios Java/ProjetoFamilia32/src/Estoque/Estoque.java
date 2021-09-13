df5196efe5b9c98ae3c2c5394f8a04f51baff000
julia <jufarias20002@gmail.com>
2021-09-13 11:57:30 -0300

    final

--- Execicios Java/ProjetoFamilia32/src/Estoque/Estoque.java
+++ Execicios Java/ProjetoFamilia32/src/Estoque/Estoque.java
@@ -1,6 +1,7 @@
 package Estoque;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -55,68 +56,7 @@ public void setTipo(String tipo) {
 	this.tipo = tipo;
 }
    
-  public List<Estoque> Adicionar(List<Estoque> estoque) {
-	  Scanner scan = new Scanner(System.in);
-	  
-	  String codProduto,nomeProduto,tipo;
-	  int qtd;
-	  System.out.println();
-//		  System.out.println("\nDigite o código do produto: ");
-//		  codProduto= scan.nextLine();
-//		 
-//		  
-//		  System.out.println("\nDigite o nome do produto: ");
-//		  nomeProduto= scan.nextLine();
-//		  
-//		  System.out.println("\nDigite o Tipo do produto :");
-//		  tipo= scan.nextLine();
-//		  
-//		  System.out.println("\nDigite a quntidade em estoque");
-//		  qtd= scan.nextInt();
-//	      scan.close();
-	  
-	       codProduto=(JOptionPane.showInputDialog(null,"Digite O código do produto :"));
-	       nomeProduto=(JOptionPane.showInputDialog(null,"Digite O nome do produto :"));
-	       tipo=(JOptionPane.showInputDialog(null,"Digite O tipo do produto :"));
-	       qtd=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite O código do produto :"));
-		 
-		   estoque.add(new Estoque(codProduto,qtd,nomeProduto,tipo));
-	 
-		   JOptionPane.showMessageDialog(null,"Produto adicionado na lista ");
-
-	  return estoque;
-  }
   
-    public List<Estoque> Remover(List<Estoque> estoque) {
-		int index=0,valor;
-		Scanner scan = new Scanner(System.in);
-		
-		for(int x=0; x < estoque.size();x ++) {
-			System.out.println("INDEX : "+index+ " - "+codigoProduto+" - "+nomeProduto+ " - "+tipo+" - "+quantidade);
-			index++;
-		}
-		
-		System.out.println("\n Qual elemento você quer remover ? ");
-		valor= scan.nextInt();
-		if(valor > estoque.size() || valor < 0) {
-			JOptionPane.showMessageDialog(null," Valor inválido");
-		}
-    	estoque.remove(valor);
-    	
-    	return estoque;
-		
-	}
-
-	public void Atualizar(List<Estoque> estoque) {
-		int index=0,valor;
-		Scanner scan = new Scanner(System.in);
-		
-		for(int x=0; x <= estoque.size();x ++) {
-			System.out.println("INDEX : "+index+ " - "+codigoProduto+" - "+nomeProduto+ " - "+tipo+" - "+quantidade);
-			index++;
-		}
-		
-	}
    
    
 }
--- Execicios Java/ProjetoFamilia32/src/Estoque/TesteEstoque.java
+++ Execicios Java/ProjetoFamilia32/src/Estoque/TesteEstoque.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -11,21 +12,93 @@ public class TesteEstoque {
 
 	public static void main(String[] args) throws IOException {
 
-	 List<Estoque> estoque = new ArrayList<>();
-	 Estoque est = new Estoque();
+	 ArrayList<Estoque> estoque = new ArrayList<>();
+	 int opc=0,qtd;
+	 String nomeProduto,tipo,codProduto;
+	 Scanner scan = new Scanner(System.in);
 	 
-	 
-	 estoque= est.Adicionar(estoque);
-	 //JOptionPane.showMessageDialog(null,"Produto adicionado na lista ");
-	
-	 
-	 estoque=est.Remover(estoque);
-	 JOptionPane.showMessageDialog(null,"Produto foi removido da lista  ");
-	 
-	 est.Atualizar(estoque);
-	 
-	 
-	 
+	 do {
+		
+		 opc=Integer.parseInt(JOptionPane.showInputDialog(null,"\n\t\t\t Menu de Opções "+
+		                                    "\n\t\t1-Adicionar itens no estoque\"+\r\n"
+		 		+ "		                     \n\t\t2-Remover itens da Lista\"+\r\n"
+		 		+ "				             \n\t\t3-Atualizar itens da Lista\"+\r\n"
+		 		+ "		                     \n\t\t4-Imprimir itens da lista\"+\r\n"
+		 		+ "				             \n\t\t5-Finalizar Programa"
+		 		+ "                          \n\nDigite uma opção :"));
+	 switch (opc) {
+	    case 1:
+	    	 System.out.println();
+	    	nomeProduto= JOptionPane.showInputDialog(null,"Digite o nome do Produto :");
+	    	tipo=JOptionPane.showInputDialog(null,"Digite o nome do tipo do Produto :");
+	    	codProduto=JOptionPane.showInputDialog(null,"Digite o código do Produto :");
+	    	qtd=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade  :"));
+	        estoque.add(new Estoque(codProduto,qtd,nomeProduto,tipo));
+	        JOptionPane.showMessageDialog(null, "Produto adicionado no estoque com sucesso!!");
+	        break;
+	        
+	    case 2:
+	    	System.out.println();
+	    	int index=0;
+	    	int aux;
+	    	
+            if(estoque.isEmpty()) {
+            	JOptionPane.showMessageDialog(null, "Impossivel remover, Lista vazia");
+            }
+	    	
+	    	System.out.println("\n\tITENS do estoque ");
+	    	for (Estoque roda : estoque) {
+				System.out.println("\nIndex :"+index+"\nCódigo do produto : "+roda.getCodigoProduto()+"\nNome : "+roda.getNomeProduto()
+                                                           +"\nTipo :"+roda.getTipo()+"\nQuantidade :"+roda.getQuantidade());
+				index++;
+			}
+	    	aux=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite qual iten você quer remover :"));
+	    	estoque.remove(aux);
+	    	JOptionPane.showMessageDialog(null, "Produto removido no estoque com sucesso!!");
+	    	break;
+	    	
+	    case 3:
+	    	System.out.println();
+	    	int index1=0;
+	    	int aux1;
+	    	
+            if(estoque.isEmpty()) {
+            	JOptionPane.showMessageDialog(null, "Impossivel Atualizar, Lista vazia");
+            }
+            System.out.println("\n\tITENS do estoque ");
+	    	for (Estoque roda : estoque) {
+				System.out.println("\nIndex :"+index1+"\nCódigo do produto : "+roda.getCodigoProduto()+"\nNome : "+roda.getNomeProduto()
+                                                           +"\nTipo :"+roda.getTipo()+"\nQuantidade :"+roda.getQuantidade());
+				index1++;
+	    	}
+				aux1=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite qual item você quer atualizar :"));
+				
+				nomeProduto= JOptionPane.showInputDialog(null,"Digite o nome do Produto :");
+		    	tipo=JOptionPane.showInputDialog(null,"Digite o nome do tipo do Produto :");
+		    	codProduto=JOptionPane.showInputDialog(null,"Digite o código do Produto :");
+		    	qtd=Integer.parseInt(JOptionPane.showInputDialog(null,"Digite a quantidade  :"));
+		        estoque.add(new Estoque(codProduto,qtd,nomeProduto,tipo));
+		        
+		        estoque.get(aux1);
+				break;
+				
+	    case 4:
+	    	System.out.println();
+	    	for (Estoque roda : estoque) {
+	    		System.out.println("\nCódigo do produto : "+roda.getCodigoProduto()+"\nNome : "+roda.getNomeProduto()
+                +"\nTipo :"+roda.getTipo()+"\nQuantidade :"+roda.getQuantidade());
+			}
+	    	break;
+	    	
+	    case 5:
+	    	JOptionPane.showMessageDialog(null, "Programa finalizado");
+	    	break;
+	    	
+	    default:
+	    	JOptionPane.showMessageDialog(null, "Opção inválida");
+	    	break;
+	   }
+	 }while(opc != 5);
 	 
 	}
 
