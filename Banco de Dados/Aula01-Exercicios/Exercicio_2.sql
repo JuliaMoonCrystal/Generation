35c2933e309f4410b891e6af2130860d0c7b9bbe
julia <jufarias20002@gmail.com>
2021-09-20 14:25:50 -0300

    Exercicio 2 e 3

---
+++ Banco de Dados/Aula01-Exercicios/Exercicio_2.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE db_ecommerce;-- Criando o bd
+USE db_ecommerce;
+
+CREATE TABLE tb_Produto(
+id BIGINT auto_increment NOT NULL,
+nome VARCHAR(100) NOT NULL,
+valor_unidade DECIMAL(7,2) NOT NULL,
+tipo_Produto VARCHAR(100) NOT NULL,
+estoque INT NOT NULL,
+PRIMARY KEY(id)
+);
+
+INSERT INTO tb_Produto(nome,valor_unidade,tipo_Produto,estoque) VALUES
+("Pen-drive",50.50,"artigo eletrônico",12000),
+("Creme Facial",40,"Produto de beleza",8000),
+("TV",5500.50,"Eletrônicos",6000),
+("NoteBook DELL",1050.50,"artigo eletrônico",2000),
+("Mouse ",29.90,"artigo eletrônico",900),
+("Jogo Uno",14.99,"Jogos",12000),
+("Horizon Forbidden West",329.90,"Games e Consoles",2000),
+("Coleira Seresto",60,"Pet Shop",6000);
+
+SELECT * FROM tb_Produto;
+
+-- valores maiores que 500
+SELECT nome,valor_unidade AS preço ,tipo_Produto AS tipo,estoque FROM tb_Produto WHERE valor_unidade > 500;
+
+-- valores menores que 500
+SELECT nome,valor_unidade AS preço ,tipo_Produto AS tipo,estoque FROM tb_Produto WHERE valor_unidade < 500;
+
+-- Atualizando um item
+UPDATE tb_Produto SET valor_unidade= 400.70 WHERE id =7;
+SELECT *FROM tb_Produto
---
+++ Banco de Dados/Aula01-Exercicios/Exercicio_3.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE db_escola;
+USE db_escola;
+
+CREATE TABLE tb_estudantes(
+id BIGINT auto_increment NOT NULL,
+nome VARCHAR(100) NOT NULL,
+ra VARCHAR(100) NOT NULL,
+sala VARCHAR(10) NOT NULL,
+serie VARCHAR(10) NOT NULL,
+nota DECIMAL(7,2) NOT NULL,
+PRIMARY KEY(id)
+);
+
+INSERT INTO tb_estudantes(nome,ra,sala,serie,nota) VALUES
+("Juliano","109228929812","102B","1-B",7.6),
+("Julia","111111111111","103B","1-A",10),
+("Fulano","907613907632","132A","1-A",3.8),
+("Thiago","107500941285","122C","1-B",9.9),
+("Josiane","185290654982","102A","1-C",7.9),
+("Allex","676767676767","100B","1-B",6.9),
+("Bruno","098765432145","121C","1-A",1.2),
+("Raj","123456789098","102B","1-C",10);
+
+SELECT * FROM tb_estudantes;
+
+-- nota maior que 7
+SELECT nome,ra,sala,serie,nota FROM tb_estudantes WHERE nota > 7;
+
+-- nota menor que 7
+SELECT nome,ra,sala,serie,nota FROM tb_estudantes WHERE nota < 7;
+
+-- atualizando uma sala
+UPDATE tb_estudantes SET sala ="102B" WHERE id =2;
+SELECT * FROM tb_estudantes;
+
+
