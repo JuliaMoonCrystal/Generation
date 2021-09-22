957fa5d41efd58de2d3971fa930c0aa887a6ccf3
julia <jufarias20002@gmail.com>
2021-09-22 16:57:25 -0300

    adicionando conteudos do Spring

---
+++ TesteSpring/demo/src/main/java/br/org/generation/hello/controller/ControllerTeste.java
@@ -0,0 +1,17 @@
+package br.org.generation.hello.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.DemoApplication;
+
+   @RestController
+public class ControllerTeste {
+   @GetMapping("/hello")
+    
+   public String sayHello() {
+	   return "Hello World!!! \n Nosso Primeiro projeto Spring";
+   }
+	
+}
