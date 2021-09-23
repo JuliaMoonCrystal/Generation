88dc96787267b7f0cad6746aaa33119c35514824
julia <jufarias20002@gmail.com>
2021-09-23 14:52:06 -0300

    Exercicio 2

---
+++ Atividades com Spring/Aula_01/hello/src/main/java/com/helloworld/hello/controller/Hello2Controler.java
@@ -0,0 +1,24 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Objetivos")
+public class Hello2Controler {
+
+	@GetMapping()
+	
+	public String hello2() {
+		String texto="<h1>Meus objetivos de aprendizagem para essa semana</h1><br>"
+				+ "<h1><ol>"
+				+ "<li> Aprender mais sobre Spring</li>"
+				+ "<li> Apresender mais sobre MySQL</li>"
+				+ "<li>Aprender mais sobre Java</li>"
+				+ "</ol></h1>";
+		
+		return texto;
+	}
+	
+}
