58a6722e82712c7d623371e87f561028bd41bc44
julia <jufarias20002@gmail.com>
2021-09-24 11:49:08 -0300

    add atualizações

--- Atividades com Spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagensController.java
+++ Atividades com Spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagensController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,13 @@ public class PostagensController {
 		return ResponseEntity.ok(repository.findAll());
 		
 	}
+	
+//	@GetMapping
+//	public ResponseEntity<Postagem> GetById(@PathVariable Long id){
+//		 return repository.findById(id)
+//		           .map(record -> ResponseEntity.ok().body(record))
+//		           .orElse(ResponseEntity.notFound().build());
+//		
+//	}
+	
 }
