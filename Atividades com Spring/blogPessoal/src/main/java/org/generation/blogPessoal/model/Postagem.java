4c9e80a8d75203a88875a66af423e520b943ba8e
julia <jufarias20002@gmail.com>
2021-09-24 10:11:53 -0300

    criando a model

---
+++ Atividades com Spring/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,67 @@
+package org.generation.blogPessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity //endica as entidades
+@Table(name ="postagem")  //indica que essa entidade será uma tabela no banco de dados
+public class Postagem {
+   
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)// esse atributo vai ser uma PK
+	private long id;
+	
+	@NotNull
+	@Size(min =5, max=100)
+    private String titulo;
+	
+	@NotNull
+	@Size(min =5, max=500)
+    private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+    private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+  
+    public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+    
+    
+}
