c2e2b970552fda70c0210bc43f40400abe9243a9
julia <jufarias20002@gmail.com>
2021-09-30 13:45:22 -0300

    ProdutoController - finalizada

--- Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/model/Produto.java
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/model/Produto.java
@@ -13,55 +13,65 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name="tb_produto")
+@Table(name = "tb_produto")
 public class Produto {
- 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-  
-  @NotNull
-  @Size(min=5,max=100)
-  private String nome;
-  
-  @NotNull
-  @Size(min=5,max=100)
-  private String nome_empresa;
-  
-  @ManyToOne
-  @JsonIgnoreProperties("produto")
-  private Categoria categoria;
-  
-  public long getId() {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String nome;
+
+	@NotNull
+	@Size(min = 5, max = 100)
+	private String nome_empresa;
+
+	@NotNull
+	private double preco;
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
 		return id;
 	}
-  
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getNome_empresa() {
 		return nome_empresa;
 	}
-	
+
 	public void setNome_empresa(String nome_empresa) {
 		this.nome_empresa = nome_empresa;
 	}
-	
+
 	public Categoria getCategoria() {
 		return categoria;
 	}
-	
+
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
-	
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 }
