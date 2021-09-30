d6de2efd5476985e06dcb7a233c00c73a4733fd5
julia <jufarias20002@gmail.com>
2021-09-30 09:26:27 -0300

    commit inicial

--- Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/ExercicioCrudApplication.java
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/ExercicioCrudApplication.java
@@ -1,4 +1,4 @@
-package org.generation.ExercicioCrud;
+package org.generation.LojaGames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
