63942bda9938e2be13e4b4524b7f6f740f6077c5
julia <jufarias20002@gmail.com>
2021-09-30 09:23:43 -0300

    commit inicaial

---
+++ Atividades com Spring/minhaLojaDeGames/src/test/java/org/generation/ExercicioCrud/ExercicioCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.ExercicioCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
