37dfd78741bd1c5fc7412d79ecf60fc05f55d7c7
julia <jufarias20002@gmail.com>
2021-09-30 09:51:16 -0300

    criando as models

---
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/model/Categoria.java
@@ -0,0 +1,71 @@
+package org.generation.LojaGames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_categoria")
+public class Categoria {
+      
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min=5,max=50)
+	private String categoria;
+	
+	@NotNull
+	@Size(min=5,max=50)
+	private String faixaEtaria;
+	
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public  String getFaixaEtaria() {
+		return faixaEtaria;
+	}
+	
+	public void setFaixaEtaria(String faixaEtaria) {
+		this.faixaEtaria = faixaEtaria;
+	}
+	
+	public List<Produto> getProduto() {
+		return produto;
+	}
+	
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
+}
---
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/model/Produto.java
@@ -0,0 +1,67 @@
+package org.generation.LojaGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+ 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+  
+  @NotNull
+  @Size(min=5,max=100)
+  private String nome;
+  
+  @NotNull
+  @Size(min=5,max=100)
+  private String nome_empresa;
+  
+  @ManyToOne
+  @JsonIgnoreProperties("produto")
+  private Categoria categoria;
+  
+  public long getId() {
+		return id;
+	}
+  
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome_empresa() {
+		return nome_empresa;
+	}
+	
+	public void setNome_empresa(String nome_empresa) {
+		this.nome_empresa = nome_empresa;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
