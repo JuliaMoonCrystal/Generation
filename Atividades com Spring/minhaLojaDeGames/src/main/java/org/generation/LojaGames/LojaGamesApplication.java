9b2c80da30af9397f0dd01bcb3075acb85b1e3e3
julia <jufarias20002@gmail.com>
2021-09-30 10:07:46 -0300

    criando os repositorys

--- Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/LojaGamesApplication.java
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/LojaGamesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExercicioCrudApplication {
+public class LojaGamesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExercicioCrudApplication.class, args);
+		SpringApplication.run(LojaGamesApplication.class, args);
 	}
 
 }
---
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package org.generation.LojaGames.repository;
+
+import java.util.List;
+
+import org.generation.LojaGames.model.Categoria;
+import org.generation.LojaGames.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List<Categoria> findAllByCategoriaContainingIgnoreCase(String categoria);
+}
