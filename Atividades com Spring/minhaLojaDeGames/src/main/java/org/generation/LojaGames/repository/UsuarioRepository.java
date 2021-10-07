9fb4c4d1ecbb285b45721de20872678398f2607f
julia <jufarias20002@gmail.com>
2021-10-07 13:35:35 -0300

    UsuarioController - finalizada

--- Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/repository/UsuarioRepository.java
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/repository/UsuarioRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.generation.LojaGames.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	public Optional<Usuario> findByUsuario(String usuario);
