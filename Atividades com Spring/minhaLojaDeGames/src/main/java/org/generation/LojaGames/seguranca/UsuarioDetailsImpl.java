3d6093e7c258ae0a139b0db4ad84960d8d5a424f
julia <jufarias20002@gmail.com>
2021-10-07 12:41:13 -0300

    UsuarioDetailsImpl - finalizada

---
+++ Atividades com Spring/minhaLojaDeGames/src/main/java/org/generation/LojaGames/seguranca/UsuarioDetailsImpl.java
@@ -0,0 +1,68 @@
+package org.generation.LojaGames.seguranca;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UsuarioDetailsImpl implements UserDetails {
+
+	private static final long serialVersionUID = 1l;
+
+	private String userName;
+	private String password;
+	private List<GrantedAuthority> authorities;
+	
+	public UsuarioDetailsImpl(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+	
+	public UsuarioDetailsImpl() {
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
