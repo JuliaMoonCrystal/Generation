363b66c652fc9e5d126a2df7032cc52b0f6429e5
julia <jufarias20002@gmail.com>
2021-10-13 15:06:31 -0300

    finalizado

--- Atividades com Spring/LojaDeJogos/src/test/java/org/generation/lojaDeJogos/repository/UsuariorepositoryTest.java
+++ Atividades com Spring/LojaDeJogos/src/test/java/org/generation/lojaDeJogos/repository/UsuariorepositoryTest.java
@@ -21,36 +21,36 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 public class UsuariorepositoryTest {
 
 	@Autowired
-	private UsuarioRepository usuarioRepository;
+	private UsuarioRepository repository;
 
 	@BeforeAll
 	void start() {
 		Usuario usuario = new Usuario(0, "João da Silva", "joao@email.com.br", "13465278");
-		if (!usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
-			usuarioRepository.save(usuario);
+		if (!repository.findByUsuario(usuario.getUsuario()).isPresent())
+			repository.save(usuario);
 		usuario = new Usuario(0, "Manuel da Silva", "manuel@email.com.br", "13465278");
 
-		if (!usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
-			usuarioRepository.save(usuario);
+		if (!repository.findByUsuario(usuario.getUsuario()).isPresent())
+			repository.save(usuario);
 		usuario = new Usuario(0, "Frederico da Silva", "frederico@email.com.br", "13465278");
-		if (!usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
-			usuarioRepository.save(usuario);
+		if (!repository.findByUsuario(usuario.getUsuario()).isPresent())
+			repository.save(usuario);
 		usuario = new Usuario(0, "Paulo Antunes", "paulo@email.com.br", "13465278");
-		if (!usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
-			usuarioRepository.save(usuario);
+		if (!repository.findByUsuario(usuario.getUsuario()).isPresent())
+			repository.save(usuario);
 	}
 
 	@Test
 	@DisplayName("💾 Retorna o nome")
 	public void findByNomeRetornaNome() {
-		Usuario usuario = usuarioRepository.findByNome("João da Silva");
+		Usuario usuario = repository.findByNome("João da Silva");
 		assertTrue(usuario.getNome().equals("João da Silva"));
 	}
 
 	@Test
 	@DisplayName("💾 Retorna 2 usuarios")
 	public void findAllByNomeContainingIgnoreCaseRetornaTresUsuarios() {
-		List<Usuario> listaDeUsuarios = usuarioRepository.findAllByNomeContainingIgnoreCase("Silva");
+		List<Usuario> listaDeUsuarios = repository.findAllByNomeContainingIgnoreCase("Silva");
 		assertEquals(3, listaDeUsuarios.size());
 	}
 
